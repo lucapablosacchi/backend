cb41fe925ae6a19658a28924afc7f77fb0742360
lucapablosacchi <sacchilucapablo@gmail.com>
2022-08-19 09:56:20 -0300

    probando herokuuuuuu

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import usuariosRouter from './routes/usuarioRoutes.js';
 import comidasRouter from './routes/comidaRoutes.js';
 
 const app = express();
+const PORT = process.env.PORT || 4000
+
 app.use(express.json())
 app.use(cors());
 
 app.use('/api/usuarios', usuariosRouter);
 app.use('/api/comidas', comidasRouter);
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
